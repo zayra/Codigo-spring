b04073f7b01d9270598976baf7e1bc589d73c8e9
zayra <cazrp24@gmail.com>
2014-02-22 12:32:58 -0600

    Comentario prueba

--- src/main/java/org/upiita/spring/jdbc/daos/HibernateUsuarioDAO.java
+++ src/main/java/org/upiita/spring/jdbc/daos/HibernateUsuarioDAO.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.upiita.spring.jdbc.entidades.Usuario;
 
+//Agregando un simple comentario
+
 @Component("usuarioDAO")
 public class HibernateUsuarioDAO implements UsuarioDAO {
 
